11824f03ee75e48c47716efba76983e57d5a00ad
daniilrusanov <daniil12099@gmail.com>
2025-02-22 22:46:58 +0200

    lr1

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+} from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { MyHeaderComponent } from "../my-header/my-header.component";
+import {IonicModule} from "@ionic/angular";
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    ExploreContainerComponent,
+    MyHeaderComponent,
+    IonicModule,
+  ],
 })
+
 export class Tab1Page {
   constructor() {}
 }
